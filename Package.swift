ed8a96279a7106028aa3d101190dd133847d32c0
Aleksandr Novikov <novikov@sketchar.io>
2026-03-20 17:07:18 +0700

    feat: add @MainActor storyboardInitCompleted overload
    Add @MainActor-isolated storyboardInitCompleted() overload for Swift 6
    strict concurrency support. The closure is wrapped via
    MainActor.assumeIsolated at invocation time.
    
    Also update Swinject dependency to sketchar fork with @MainActor support.

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         .library(name: "SwinjectStoryboard", targets: ["SwinjectStoryboard"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Swinject/Swinject.git", .upToNextMajor(from: "2.7.1")),
+        .package(url: "https://github.com/sketchar/Swinject.git", .upToNextMajor(from: "2.13.0")),
     ],
     targets: [
         .target(
